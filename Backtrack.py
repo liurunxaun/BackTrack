f7a38b63a4103b54c17792ca003d5bf0362fb0b9
liurunxuan <94226565+liurunxaun@users.noreply.github.com>
2024-11-26 09:58:21 +0800

    init

---
+++ Backtrack.py
@@ -0,0 +1,67 @@
+import extract as extract
+import back as back
+import forward as forward
+import answer as answer
+from neo4j import GraphDatabase
+import time
+
+
+def main(question, max_pop, schema_text_path, driver):
+    print(f"\n问题:{question}")
+
+    # 1. 从问题中提取条件实体、目的实体、实体类型
+    print("\n======1. 从问题中提取条件实体、目的实体、实体类型======")
+    conditions, aims = extract.extract(question)
+
+    if len(conditions) != 0 and len(aims) != 0:
+        print(f"conditions:{conditions}")
+        print(f"aims:{aims}")
+    else:
+        print("问题中不包含知识图谱范围内的条件和目的")
+        print("\n======2. 调用大模型生成最终答案======")
+        generation = answer.generate_answer(question)
+        return generation
+
+
+    # 2. 倒推找抽象本体推理路径
+    print("\n======2.倒推找抽象本体推理路径======")
+    paths = back.back(conditions, aims, max_pop, schema_text_path)
+
+    if len(paths) != 0:
+        print("抽象本体推理路径:")
+        for path in paths:
+            print(" -> ".join(path))
+    else:
+        print("没找到抽象本体推理路径。请尝试换个说法，或者描述的更详细一些")
+        print("\n======3. 调用大模型生成最终答案======")
+        generation = answer.generate_answer(question)
+        return generation
+
+    # 3. 正推找具体实体推理路径，筛选得到条件和目的实体
+    print("\n======3. 正推具体实体推理路径，筛选得到条件和目的实体======")
+    reference = forward.forward(paths, conditions, driver, aims)
+
+    if reference != "":
+        print(reference)
+    else:
+        print("没有匹配到实体")
+
+    # 4. 调用大模型生成最终答案
+    print("======4. 调用大模型生成最终答案======")
+    generation = answer.generate_answer(question, reference)
+    return generation
+
+
+if __name__ == "__main__":
+    question = "SpellGCN的作者" # 用户输入的问题
+    max_pop = 5 # 构建推理树时最大的推理跳数
+    schema_text_path = "data/schema.txt" # 所用知识图谱的关系定义文件，格式是：label:entity_name-relation-label:entity_name
+    uri = "bolt://10.43.108.62:7687"  # Neo4j连接URI
+    user = "neo4j"  # 用户名
+    password = "12345678"  # 密码
+    driver = GraphDatabase.driver(uri, auth=(user, password)) # 创建数据库连接
+
+    time0 = time.time()
+    final_answer = main(question, max_pop, schema_text_path, driver)
+    time1 = time.time()
+    print(f"answer:\n{final_answer}\n用时:{time1-time0}")
---
+++ extract.py
@@ -0,0 +1,64 @@
+from utils import llm_api as llm
+
+
+def extract(question):
+    """
+    输入：用户的问题
+    处理过程：使用大模型提取条件实体、目的实体、实体类型。
+            处理大模型的答复，为condition_entity, condition_label, aim_entity, aim_label赋值
+    输出：conditions:[entity, label], aims:[entity, label]
+    """
+
+    query = f"""
+    我做的是一个知识图谱增强的知识问答系统。知识图谱是由从论文中提取的要素组成的，节点包括作者姓名，方法，数据集等。
+    用户的输入是：{question}
+    你需要从用户输入的问题中提取出问题的条件实体及其类型，目的实体及其类型。
+    条件是指问题中的已知条件。目的是指问题中想问的东西。
+    或许从语义上来讲条件和目的会很复杂，但是你只需要从下表中选择,括号里是解释的内容,实体类型必须是这里面的原词:作者（作者姓名）, 标题（论文标题）, 机构（作者所在机构）, 领域（论文所属领域）, 会议（论文发表的会议或期刊名称）, 关键词（论文关键词）, 问题（论文所解决的问题）, 方法（论文中提出的方法）, 模型（论文中使用的模型的名称）, 任务（论文针对的任务）, 数据集（论文实验所使用的数据集）, 创新点（论文的创新点）, 指标（论文实验所使用的指标）, id（论文在数据库中的id）
+
+    下面是几个例子：
+    input:Kausalgie发表过多少篇论文？
+    条件实体有:Kausalgie(作者)。目的实体有:papers(标题)
+    output:Kausalgie,作者.papers,标题
+    
+    input:machine translation领域有哪些数据集？
+    条件实体有:machine translation(领域)。目的实体有:dataset(数据集)
+    output:machine translation,领域.dataset,数据集
+
+    input:SpellGCN的作者是谁？
+    条件实体有:SpellGCN (标题)。目的实体有:author(作者)
+    output:SpellGCN,标题.author,作者
+
+    input:MindMap和KAG哪个更早？作者分别是谁？
+    条件实体有:MindMap (方法);KAG (方法)。目的实体有:earlier(none);author(作者)
+    output:MindMap,方法;KAG,方法.earlier,none;author,作者
+    
+    input:In the field of machine translation, what are the ways to obtain and generate appropriate data sets for low-resource situations?
+    条件实体有:machine translation(领域);low-resource situations(问题);obtain and generate appropriate data sets(问题)。目的实体有:ways(方法)
+    output:machine translation,领域;low-resource situations,问题;obtain and generate appropriate data sets,问题.ways,方法
+
+    括号里是实体类型。如果从表中找不到就写none
+    输出是output:后面的。不需要写"output:"
+    """
+
+    response = llm.spark_4_0(query)
+
+    conditions = []
+    aims = []
+
+    try:
+        split = response.split(".")
+
+        split1 = split[0].split(";")
+        for item in split1:
+            item_split = item.split(",")
+            conditions.append([ item_split[0], item_split[1] ]) # 0是实体，1是实体类型
+
+        split2 = split[1].split(";")
+        for item in split2:
+            item_split = item.split(",")
+            aims.append([ item_split[0], item_split[1] ]) # 0是实体，1是实体类型
+    except:
+        return [],[]
+
+    return conditions, aims
