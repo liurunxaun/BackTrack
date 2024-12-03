48ead12074b8a7e939f76e9982b0e18f8111fc26
liurunxuan <94226565+liurunxaun@users.noreply.github.com>
2024-12-03 10:55:56 +0800

    1

--- test.py
+++ test.py
@@ -62,7 +62,7 @@ def evaluate_and_save_results(df, method, max_pop, top_k, model, label_dict, dri
 
         # 判断要使用哪种指标
         if metric == "BERTScore":
-            bertScore_model_path = "evaluation_metrics/bert-large-uncased"
+            bertScore_model_path = "./evaluation_metrics/bert-large-uncased"
             # Compute BERTScore
             P, R, F1 = score(cand, ref, lang="en", verbose=True, model_type=bertScore_model_path,
                              rescale_with_baseline=True, device='cuda')
@@ -123,13 +123,12 @@ if __name__ == "__main__":
     method = "BackTrack"  # 选择要使用的方法，包括"BackTrack"倒推，"RuleBase"基于规则
     max_pop = 5  # 构建推理树时最大的推理跳数
     top_k = 5  # 如果一个实体满足next_label的邻居有多个，最多取top_k个
-    model = "gpt-4o-mini"  # 选择生成最终答案使用的模型。包括：spark, gpt-4o-mini（提取条件和目的就使用spark，因为便宜，而且效果也还不错）
+    model = "spark"  # 选择生成最终答案使用的模型。包括：spark, gpt-4o-mini（提取条件和目的就使用spark，因为便宜，而且效果也还不错）
     schema_text_path = "./data/IFLYTEC-NLP/GraphKnowledge/schema.txt"  # 所用知识图谱的关系定义文件，格式是：label:entity_name-relation-label:entity_name
-    output_dir = "./output"
-    test_dataset = "./data/IFLYTEC-NLP/test200/200_QA_多文档.csv"
-    timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
-    metric = "BERTScore"
-
+    test_dataset = "./data/IFLYTEC-NLP/test200/200_QA_多文档.csv"  # 要进行测试的数据集路径，注意是csv格式的
+    output_dir = "./output"  # 测试结果的存储路径，存储为csv文件，会根据上面的参数和当前时间命名
+    timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")  # 指定输出文件名中的时间戳
+    metric = "BERTScore"  # 选择实验的指标，包括：BERTScore
 
     label_dict = back.build_label_dict(schema_text_path)
     df = pd.read_csv(test_dataset)
